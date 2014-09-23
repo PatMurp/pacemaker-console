0bfdc5ee2305bf7ede41c107fbd9da18551737e3
patmurphy <patmurphy@10.20.13.49>
2014-09-23 11:06:53 +0100

    Change main method and add cliche and guava jars

--- src/controllers/Main.java
+++ src/controllers/Main.java
@@ -1,37 +1,39 @@
 package controllers;
 
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.util.ArrayList;
-import java.util.List;
-
-import com.thoughtworks.xstream.XStream;
-import com.thoughtworks.xstream.io.xml.DomDriver;
-
-import utils.FileLogger;
-import models.User;
+import asg.cliche.Command;
+import asg.cliche.Param;
+import asg.cliche.Shell;
+import asg.cliche.ShellFactory;
 
 public class Main
 {
-	 public static void main(String[] args) throws IOException
-	  {
-	    FileLogger logger = FileLogger.getLogger();
-	    logger.log("Creating user list");
+  @Command
+  public void createUser (String firstName,  String lastName, 
+                          String email,      String password)
+  {
+  }
 
-	    List<User> users = new ArrayList<User>();
-	    users.add(new User("Bart", "Simpson", "bart@simpson.com", "secret"));
-	    users.add(new User("Homer", "Simpson", "bart@simpson.com", "secret"));
-	    users.add(new User("Lisa", "Simpson", "bart@simpson.com", "secret"));
-	    System.out.println(users);
-	    
-	    logger.log("Serializing contacts to XML");
-	    XStream xstream = new XStream(new DomDriver());
-	    ObjectOutputStream out = xstream.createObjectOutputStream(new FileWriter("users.xml"));
-	    out.writeObject(users);
-	    out.close();  
+  @Command
+  public void getUser (String email)
+  {
 
-	    logger.log("Finished - shutting down");
-	  }
+  }
+
+  @Command
+  public void getUsers ()
+  {
+
+  }
+
+  @Command
+  public void deleteUser (String email)
+  {
+  }
+
+  public static void main(String[] args) throws IOException
+  {
+    Shell shell = ShellFactory.createConsoleShell("pc", "Welcome to pcemaker-console - ?help for instructions", new Main());
+    shell.commandLoop(); 
+  }
 }
-
