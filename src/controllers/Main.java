db71b31241d9265b7aa06e3019eb75e2eef6e688
patmurphy <patmurphy@192.168.1.103>
2014-10-01 10:44:28 +0100

    Add Activity and Location models Add commands to all an activity and location

--- src/controllers/Main.java
+++ src/controllers/Main.java
@@ -1,12 +1,12 @@
 package controllers;
 
-
 import java.io.IOException;
 import java.util.Collection;
 import java.util.List;
 
 import com.google.common.base.Optional;
 
+import models.Activity;
 import models.User;
 import asg.cliche.Command;
 import asg.cliche.Param;
@@ -15,43 +15,68 @@ import asg.cliche.ShellFactory;
 
 public class Main
 {
-	PacemakerAPI paceApi = new PacemakerAPI();
-	
-	@Command(description="Create a new User")
-	  public void createUser (@Param(name="first name") String firstName, @Param(name="last name") String lastName, 
-	                          @Param(name="email")      String email,     @Param(name="password")  String password)
-	  {
-		paceApi.createUser(firstName, lastName, email, password);
-	  }
-	
-	  @Command(description="Get a Users details")
-	  public void getUser (@Param(name="email") String email)
-	  {
-		  User user = paceApi.getUserByEmail(email);
-		  System.out.println(user);
-	  }
-	
-	  @Command(description="Get all users details")
-	  public void getUsers ()
-	  {
-		  Collection<User> users = paceApi.getUsers();
-		  System.out.println(users);
-	  }
-	
-	  @Command(description="Delete a User")
-	  public void deleteUser (@Param(name="email") String email)
-	  {
-		  Optional<User> user = Optional.fromNullable(paceApi.getUserByEmail(email));
-		  if (user.isPresent())
-		  {
-			  paceApi.deleteUser(user.get().id);
-		  }
-		  
-	  }
+  PacemakerAPI paceApi = new PacemakerAPI();
+
+  @Command(description = "Create a new User")
+  public void createUser(@Param(name = "first name") String firstName, @Param(name = "last name") String lastName,
+      @Param(name = "email") String email, @Param(name = "password") String password)
+  {
+    paceApi.createUser(firstName, lastName, email, password);
+  }
+
+  @Command(description = "Get a Users details")
+  public void getUser(@Param(name = "email") String email)
+  {
+    User user = paceApi.getUserByEmail(email);
+    System.out.println(user);
+  }
+
+  @Command(description = "Get all users details")
+  public void getUsers()
+  {
+    Collection<User> users = paceApi.getUsers();
+    System.out.println(users);
+  }
+
+  @Command(description = "Delete a User")
+  public void deleteUser(@Param(name = "email") String email)
+  {
+    Optional<User> user = Optional.fromNullable(paceApi.getUserByEmail(email));
+    if (user.isPresent())
+    {
+      paceApi.deleteUser(user.get().id);
+    }
+
+  }
+
+  @Command(description = "Add an activity")
+  public void addActivity(@Param(name = "user-id") Long id, @Param(name = "type") String type,
+      @Param(name = "location") String location, @Param(name = "distance") double distance)
+  {
+    Optional<User> user = Optional.fromNullable(paceApi.getUser(id));
+    if (user.isPresent())
+    {
+      paceApi.createActivity(id, type, location, distance);
+    }
+  }
+
+  @Command(description = "Add Location to activiy")
+  public void addLocation(@Param(name = "activity-id") Long id, @Param(name = "latitude") float latitude,
+      @Param(name = "longitude") float longitude)
+  {
+    Optional<Activity> activity = Optional.fromNullable(paceApi.getActivity(id));
+    if (activity.isPresent())
+    {
+      paceApi.addLocation(activity.get().id, latitude, longitude);
+      ;
+    }
+
+  }
 
   public static void main(String[] args) throws IOException
   {
-    Shell shell = ShellFactory.createConsoleShell("pc", "Welcome to pcemaker-console - ?help for instructions", new Main());
-    shell.commandLoop(); 
+    Shell shell = ShellFactory.createConsoleShell("pc", "Welcome to pcemaker-console - ?help for instructions",
+        new Main());
+    shell.commandLoop();
   }
 }
--- src/models/User.java
+++ src/models/User.java
@@ -1,47 +1,52 @@
 package models;
 
 import static com.google.common.base.Objects.toStringHelper;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import com.google.common.base.Objects;
 
-public class User 
+public class User
 {
-	static Long counter = 0l;
-	
-	public Long id;
-	
+
+  static Long counter = 0l;
+
+  public Long id;
+
   public String firstName;
   public String lastName;
   public String email;
   public String password;
 
+  // public List<Activity> activities = new ArrayList<>();
+  public Map<Long, Activity> activities = new HashMap<>();
+
   public User()
   {
   }
-  
- 
+
   public User(String firstName, String lastName, String email, String password)
   {
-	this.id = counter++;
+    this.id = counter++;
+
     this.firstName = firstName;
     this.lastName = lastName;
     this.email = email;
     this.password = password;
   }
-  
+
   @Override
   public String toString()
   {
-	  return toStringHelper(this).addValue(firstName)
-			  					.addValue(lastName)
-			  					.addValue(password)
-			  					.addValue(email)
-			  					
-			  					.toString();
+    return toStringHelper(this).addValue(id).addValue(firstName).addValue(lastName).addValue(password).addValue(email)
+        .addValue(activities)
+        .toString();
   }
-  
+
   @Override
   public int hashCode()
   {
-	  return Objects.hashCode(this.lastName, this.firstName, this.email, this.password);
+    return Objects.hashCode(this.lastName, this.firstName, this.email, this.password);
   }
 }
